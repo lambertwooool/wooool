e8887ab27ead0fe480ff770785967f6bdd10c07d
lambertwang <95608336+lambertwooool@users.noreply.github.com>
2024-04-04 22:25:37 +0800

    support bria rmbg

--- modules/controlnet/bria_rmbg/__init__.py
+++ modules/controlnet/bria_rmbg/__init__.py
@@ -9,19 +9,19 @@ import modules.paths
 from modules.model import model_helper, model_loader, model_patcher
 from modules.util import HWC3, image_pad
 
-def get_mask(rmbgmodel, input_image, device):
+def get_mask(rmbgmodel, input_image):
     h, w = input_image.shape[:2]
     im_tensor = torch.tensor(input_image, dtype=torch.float32).permute(2,0,1)
     im_tensor = torch.unsqueeze(im_tensor,0)
     im_tensor = torch.divide(im_tensor,255.0)
     im_tensor = normalize(im_tensor,[0.5,0.5,0.5],[1.0,1.0,1.0])
-    im_tensor.to(device)
+    im_tensor = im_tensor.to(next(rmbgmodel.parameters()))
 
     result = rmbgmodel(im_tensor)
     result = torch.squeeze(F.interpolate(result[0][0], size=(h, w), mode='bilinear') ,0)
     ma = torch.max(result)
     mi = torch.min(result)
-    mask = ((result-mi)/(ma-mi)).permute(1,2,0).numpy()
+    mask = ((result-mi)/(ma-mi)).cpu().permute(1,2,0).numpy()
     np_img = (mask * input_image + 255 * (1 - mask)).astype(np.uint8)
     mask = (mask * 255).astype(np.uint8)
     
@@ -33,21 +33,20 @@ class BriaRemoveBackgroundDetector:
         model_path = os.path.join(modules.paths.annotator_models_path, filename)
 
         model = BriaRMBG()
-        state_dict = model_helper.load_torch_file(model_path)
-        model.load_state_dict(state_dict)
-
         load_device, offload_device, dtype, manual_cast_dtype = model_loader.get_device_and_dtype("annotator")
+        state_dict = model_helper.load_torch_file(model_path, dtype=dtype)
+        model.load_state_dict(state_dict)
+        model.to(dtype)
 
         self.model = model_patcher.ModelPatcher(model, load_device, offload_device)
 
 
     def __call__(self, input_image):
         input_image, remove_pad = image_pad(input_image)
-        device = self.model.load_device
 
         with torch.no_grad():
             model_loader.load_model_gpu(self.model)
-            mask, detected_map = get_mask(self.model.model, input_image, device)
+            mask, detected_map = get_mask(self.model.model, input_image)
         
         detected_map = remove_pad(HWC3(detected_map))
 
